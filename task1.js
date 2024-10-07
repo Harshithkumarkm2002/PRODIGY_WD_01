c0b993fbc44fa65e3105d5443dff25b193a8a137
Harshith Kumar K M <143926169+Harshithkumarkm2002@users.noreply.github.com>
2024-10-07 16:30:11 +0530

    Delete task1.js

--- task1.js
+++
@@ -1,9 +0,0 @@
-// Change navbar background color on scroll
-window.onscroll = function() {
-    const navbar = document.getElementById('navbar');
-    if (window.scrollY > 100) {
-        navbar.style.backgroundColor = '#111';
-    } else {
-        navbar.style.backgroundColor = '#333';
-    }
-};
